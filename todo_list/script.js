6ded9553a72b82dbe3a4e195ca7bfce4e9fa968f
Margarita Sirbu <margaritapetru@hotmail.com>
2022-02-15 22:33:24 -0800

    To-Do list: add, remove & mark complete

---
+++ todo_list/script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+    if (todo) {
+        todoText = todo.text;
+    }
+    if (todoText) {
+        const todoEl = document.createElement('li');
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
+        todoEl.innerText = todoText;
+        //mark as complete by clicking on the element
+        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed')); 
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            todoEl.remove();
+        })
+        todosUL.appendChild(todoEl);
+        input.value = '';
+    }
+}
